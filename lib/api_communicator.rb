4b5e3223669e7fd42bd4dc2d384225e637296216
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-18 15:15:54 -0400

    Group lab complete

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,29 +2,27 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+STAR_WARS_API_URL = 'http://www.swapi.co/api/people/'
+
 def get_character_movies_from_api(character)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+
+  response_hash = api_request(STAR_WARS_API_URL)
+
+  character_info = response_hash["results"].find {|name| name['name'] == character }
+
+  film_urls = character_info["films"]
   
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
+  film_urls.map do |film_url|
+    api_request(film_url)
+  end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  character_info = response_hash["results"].find(|name| name['name'] == character }
-
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    puts film["title"]
+  end
 end
 
 def show_character_movies(character)
@@ -36,3 +34,15 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+def api_request(url)
+  response_string = RestClient.get(url)
+  JSON.parse(response_string)
+end
+
+
+
+
+
+
+
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -5,5 +5,5 @@ end
 
 def get_character_from_user
   puts "Please enter a character: "
-  gets.chomp.downcase
+  gets.chomp
 end
