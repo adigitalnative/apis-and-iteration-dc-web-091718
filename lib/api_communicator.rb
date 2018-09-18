559facbfd9a61143410b2a24e524b10020de9b65
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-18 13:50:33 -0400

    Pre-lecture pause point

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -10,9 +10,10 @@ def get_character_movies_from_api(character)
   # NOTE: in this demonstration we name many of the variables _hash or _array. 
   # This is done for educational purposes. This is not typically done in code.
 
-
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+  character_info = response_hash["results"].find(|name| name['name'] == character }
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
